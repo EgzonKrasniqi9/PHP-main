e20122c75c5a6f79ba5e5217a1ad77578c714256
EgzonKrasniqi9 <egzon2008krasniqi1@gmail.com>
2025-09-11 17:45:04 +0200

    comit
    comit

--- lesson4/staticVariable.php
+++ lesson4/staticVariable.php
@@ -2,12 +2,12 @@
 function callCounter(){
     static $count = 0;
     $count++;
-    echo "The value of count variab;e is: $count <br>"
+    echo "The value of count variab;e is: $count <br>";
 }
 
-callCounter()
-callCounter()
-callCounter()
-callCounter()
-callCounter()
+callCounter();
+callCounter();
+callCounter();
+callCounter();
+callCounter();
 ?>
