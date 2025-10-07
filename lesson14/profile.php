90da5a2f137428b6451e66439207ad2fcf6589d0
EgzonKrasniqi9 <egzon2008krasniqi1@gmail.com>
2025-10-07 17:47:10 +0200

    comit

--- lesson14/profile.php
+++ lesson14/profile.php
@@ -1,23 +1,25 @@
-<?php include("header.php"); 
+<?php 
+session_start();
+	include_once('config.php');
 
-if(empty($_SESSION["username"]))
-{
-	header('Location: login.php');
-}
-$id = $_GET['id'];
-$id = "SELECT * FROM users WHERE id=:id";
-$selectUsers->bindParam(':id', $id)
-$selectUsers=$conn->prepare($sql);
+	if(empty($_SESSION['username']))
+	{
+		header('Location: login.php');
+	}
 
-$selectUsers->execute();
+  $id = $_GET['id'];
+   $sql = "SELECT * FROM users WHERE id=:id";
+   $selectUser = $conn->prepare($sql);
+   $selectUser->bindParam(':id', $id);
+   $selectUser->execute();
 
-$users_data = $selectUsers->fetchAll();
-
+   $user_data = $selectUser->fetch();
 ?>
+
 <?php include("header.php"); ?>
+	
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Welcome, <i> <?php echo $_SESSION['username']; ?> </i></a>
-
 
   <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
@@ -25,7 +27,6 @@ $users_data = $selectUsers->fetchAll();
     </li>
   </ul>
 </nav>
-
 
 <div class="container-fluid">
   <div class="row">
@@ -47,11 +48,10 @@ $users_data = $selectUsers->fetchAll();
     
         </ul>
 
-
       </div>
     </nav>
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
@@ -64,20 +64,17 @@ $users_data = $selectUsers->fetchAll();
               <span class="text-muted" for='id'>Id</span>
               <input  type="number" class="form-control" id="floatingInput" placeholder="Id" name="id" value="<?php echo  $user_data['id'] ?>" readonly>
 
-
               <span class="text-muted" for='name'> Name </span>
               <input class="form-control" type="text" name="name" value="<?php echo $user_data['name'] ?>" required><br>
+
               <span class="text-muted"> Surname </span>
               <input class="form-control" type="text" name="surname" value="<?php echo$user_data['surname'] ?>" required><br>
-
 
               <span class="text-muted"> Username </span>
               <input class="form-control" type="text" name="username" value="<?php echo $user_data['username'] ?>" required><br>
 
-
               <span class="text-muted">Email</span>
               <input class="form-control" type="email" name="email" value="<?php echo $user_data['email'] ?>" required><br>
-
 
               <span class="text-muted">Password</span>
               <input class="form-control" type="password" name="password" required><br><br>
@@ -90,6 +87,5 @@ $users_data = $selectUsers->fetchAll();
     </main>
   </div>
 </div>
-
 
 <?php include("footer.php"); ?>
